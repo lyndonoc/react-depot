5f8ee3d818314babf7c60ed554aaa9773ea9080e
lyndonoc <lyndonoc@gmail.com>
2020-03-11 22:55:38 -0400

    github link updated

--- src/demo/components/octocat/index.js
+++ src/demo/components/octocat/index.js
@@ -4,7 +4,7 @@ const Octocat = () => {
   return (
     <a
       className="octocat-icon"
-      href="https://github.com"
+      href="https://github.com/lyndonoc/react-form-contractor"
     >
       <svg
         aria-hidden="true"
