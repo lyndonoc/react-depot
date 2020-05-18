fda032d92d2acbd9d3e4f877a055e6e2f76c7a29
lyndonoc <lyndonoc@gmail.com>
2020-05-18 18:04:01 -0400

    Updates

---
+++ precache-manifest.de5a41e8748b470efe1abc4e38747146.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3cb49ec32049c487b7300848f2172d",
+    "url": "https://lyndonoc.github.io/react-depot/index.html"
+  },
+  {
+    "revision": "7c6cc9607ed2548d022e",
+    "url": "https://lyndonoc.github.io/react-depot/static/css/main.a2509b0c.chunk.css"
+  },
+  {
+    "revision": "43c8ea5dfe495d1084e5",
+    "url": "https://lyndonoc.github.io/react-depot/static/js/2.4fc2f8b7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "https://lyndonoc.github.io/react-depot/static/js/2.4fc2f8b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6cc9607ed2548d022e",
+    "url": "https://lyndonoc.github.io/react-depot/static/js/main.34cbf119.chunk.js"
+  },
+  {
+    "revision": "f061bd02e2ccedbd6136",
+    "url": "https://lyndonoc.github.io/react-depot/static/js/runtime-main.6ec4be4b.js"
+  }
+]);
